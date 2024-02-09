8f5a1fa1348baccbd4c666611a0e5bd01b7a9e9b
Angelina Bezik <angelina1337bz@gmail.com>
2024-02-09 20:39:56 +0300

    feat: add solution for shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -440,8 +440,22 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let newSrt = str;
+  for (let i = 1; i <= iterations; i += 1) {
+    let oneStr = '';
+    let twoStr = '';
+    for (let j = 0; j < str.length; j += 2) {
+      oneStr += newSrt[j];
+      twoStr += newSrt[j + 1];
+    }
+    newSrt = oneStr + twoStr;
+    if (newSrt === str) {
+      const newIteration = iterations % i;
+      return shuffleChar(str, newIteration);
+    }
+  }
+  return newSrt;
 }
 
 /**
